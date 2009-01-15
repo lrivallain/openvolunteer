920368e8124ba05928da2afddc40527d914e498a
ludovic.rivallain <ludovic.rivallain@9933320a-dfdd-11dd-987e-d7c0a4ebcf36>
2009-01-15 07:39:27 +0000

    * Adding copyright and GPLv3 notices   * Headers in python files   * 'COPYING' file = GPL

--- models.py
+++ models.py
@@ -1,6 +1,28 @@
 #-*- coding: utf-8 -*-
-# -- deprecated with MEDIA_ROOT use --
-#from ovsettings import *
+"""
+    ---------------------------------------------------------------------------
+
+                               OpenVolunteer
+                     Copyright 2009, Ludovic Rivallain
+
+    ---------------------------------------------------------------------------
+    This file is part of OpenVolunteer.
+
+    OpenVolunteer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenVolunteer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenVolunteer.  If not, see <http://www.gnu.org/licenses/>.
+    ---------------------------------------------------------------------------
+"""
+
 from django.db import models
 from django.utils.dates import *
 
